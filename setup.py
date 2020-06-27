0616b0f67de24011e5446900ba86076813d6c7dc
Bobrock <barret.obrock@gmail.com>
2020-06-26 19:42:40 -0500

    Refactor package management scripts

--- setup.py
+++ setup.py
@@ -4,34 +4,39 @@
 Setup CAH bot.
 """
 import os
+import yaml
 import versioneer
 from setuptools import setup, find_packages
 
 
+# Read in package info via YAML
+with open('config.yaml') as f:
+    package_info = yaml.load(f, Loader=yaml.FullLoader)
+
+PACKAGE = package_info['REPO']['NAME']
+DESC = package_info['REPO']['DESC']
+URL = package_info['REPO']['URL']
+DEPS = package_info['REPO']['DEPS']
 here_dir = os.path.abspath(os.path.dirname(__file__))
-init_fp = os.path.join(here_dir, *['cah_bot', '__init__.py'])
+init_fp = os.path.join(here_dir, *[PACKAGE, '__init__.py'])
+
+# Package Requirements
+fpath = os.path.join(here_dir, 'requirements.txt')
+with open(fpath, 'r') as f:
+    reqs_raw = f.read()
+reqs_list = reqs_raw.strip().split('\n')
 
 setup_args = {
-    'name': 'cah_bot',
+    'name': PACKAGE,
     'version': versioneer.get_version(),
     'cmdclass': versioneer.get_cmdclass(),
-    'license': 'MIT',
-    'description': 'A library for playing Cards Against Humanity on Slack',
-    'url': 'https://github.com/barretobrock/cah_bot',
+    'license': 'GPL',
+    'description': DESC,
+    'url': URL,
     'author': 'Barret Obrock',
-    'author_email': 'barret@barretobrock.ee',
     'packages': find_packages(exclude=['tests']),
-    'dependency_links': [
-        'https://github.com/barretobrock/slacktools/tarball/master#egg=slacktools'
-    ],
-    'install_requires': [
-        'Flask==1.1.2',
-        'requests>=2.20.0'
-        'numpy==1.18.4',
-        'pandas==1.0.3',
-        'slacktools',
-        'slackeventsapi==2.1.0',
-    ]
+    'dependency_links': DEPS,
+    'install_requires': reqs_list
 }
 
 setup(**setup_args)
--- update_script.sh
+++ update_script.sh
@@ -1,48 +1,6 @@
 #!/usr/bin/env bash
-#/      --update_script.sh--
-#/  Pulls changes from remote master and then updates the local python package
-#/
-#/  Usage: update_script.sh [options]
-#/
-#/  Options
-#/      -s|--skip-deps                      Skips update of dependencies.
-#/      -v|--version                        Prints script name & version.
-#/
+# Calls the update_script file at whatever path the py-package-manager repo is in
+PPM_PATH=${HOME}/extras/py-package-manager/update_script.sh
 
-# DEFAULT VARIABLES
-# ------------------------------------------
-NAME="Repo Update Script"
-VERSION="0.0.1"
-SKIP_DEPS=0
-
-# Import common variables / functions
-source ./common.sh
-
-# REPO-SPECIFIC VARIABLES
-# ------------------------------------------
-REPO=cah_bot
-GIT_URL=git+https://github.com/barretobrock/${REPO}.git#egg=${REPO}
-# DIRECTORY
-REPO_DIR=${HOME}/extras/${REPO}
-
-NODEPS_FLAG=''
-if [[ "${SKIP_DEPS}" == "1" ]];
-then
-    echo "Not pip installing dependencies"
-    NODEPS_FLAG="--no-deps"
-fi
-
-# GIT PULL
-# ------------------------------------------
-announce_section "Pulling update from git repo"
-# TODO see if I can check if master is up to date before issuing command. If it is, don't pull
-(cd ${REPO_DIR} && git pull origin master)
-
-# PY PACKAGE UPDATE
-# ------------------------------------------
-# Then update the python package locally
-announce_section "Beginning update of ${REPO}"
-# TODO check if installed, then upgrade if so
-python3 -m pip install ${GIT_URL} --upgrade ${NODEPS_FLAG}
-
-announce_section "Process completed"
+ADDL="${@}" # Option for passing additional commands onwards
+sh ${PPM_PATH} --config $(pwd)/config.yaml ${ADDL}
