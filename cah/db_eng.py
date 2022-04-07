a4723b48636afcb86ec16ae8806cadda36700ae1
bobrock <barret@tarkmail.com>
2022-04-07 16:28:10 -0500

    Add logging to engine methods

--- cah/db_eng.py
+++ cah/db_eng.py
@@ -1,5 +1,6 @@
 import traceback
 from typing import (
+    Any,
     Dict,
     List,
     Optional,
@@ -10,7 +11,10 @@ from sqlalchemy.engine import (
     create_engine,
     URL
 )
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import (
+    sessionmaker,
+    Session
+)
 from easylogger import Log
 from cah.model import (
     SettingType,
@@ -25,6 +29,7 @@ class WizzyPSQLClient:
     """Creates Postgres connection engine"""
 
     def __init__(self, props: Dict, parent_log: Log, **kwargs):
+        _ = kwargs
         self.log = Log(parent_log, child_name=self.__class__.__name__)
         self.engine = create_engine(URL.create(
             drivername='postgresql+psycopg2',
@@ -51,17 +56,23 @@ class WizzyPSQLClient:
 
     def get_setting(self, setting: SettingType) -> Optional[Union[int, bool]]:
         """Attempts to return a given setting"""
+        self.log.debug(f'Received request for setting: {setting.name}')
         with self.session_mgr() as session:
             result = session.query(TableSetting).filter(TableSetting.setting_type == setting).one_or_none()
             if result is None:
+                self.log.debug('Setting was None.')
                 return result
+
             if setting.name.startswith('IS_'):
                 # Boolean
+                self.log.debug(f'Returning: {result.setting_int == 1}')
                 return result.setting_int == 1
+            self.log.debug(f'Returning: {result.setting_int}')
             return result.setting_int
 
     def set_setting(self, setting: SettingType, setting_val: Union[int, bool]):
         """Attempts to set a given setting"""
+        self.log.debug(f'Received request to set setting: {setting.name} to {setting_val}')
         with self.session_mgr() as session:
             session.query(TableSetting).filter(TableSetting.setting_type == setting).update(
                 {TableSetting.setting_int: setting_val}
@@ -69,6 +80,7 @@ class WizzyPSQLClient:
 
     def get_player_from_hash(self, user_hash: str) -> Optional[TablePlayer]:
         """Takes in a slack user hash, outputs the expunged object, if any"""
+        self.log.debug(f'Received request to fetch player: {user_hash}')
         with self.session_mgr() as session:
             user = session.query(TablePlayer).filter(TablePlayer.slack_user_hash == user_hash).one_or_none()
             if user is not None:
@@ -77,6 +89,7 @@ class WizzyPSQLClient:
 
     def get_active_players(self) -> List[TablePlayer]:
         """Retrieves a list of active players"""
+        self.log.debug('Received request to fetch all active players')
         with self.session_mgr() as session:
             players = session.query(TablePlayer).filter(TablePlayer.is_active).all()
             session.expunge_all()
@@ -84,6 +97,7 @@ class WizzyPSQLClient:
 
     def set_active_players(self, player_hashes: List[str]):
         """Retrieves a list of active players, sets them as active and anyone not in that list as inactive"""
+        self.log.debug(f'Received request to set {len(player_hashes)} players as active.')
         with self.session_mgr() as session:
             players = session.query(TablePlayer).all()
             player: TablePlayer
@@ -91,17 +105,27 @@ class WizzyPSQLClient:
                 player.is_active = player.slack_user_hash in player_hashes
             session.add_all(players)
 
-    def refresh_table_object(self, tbl_obj):
-        """Refreshes a table object by adding it to the session and """
-        with self.session_mgr() as session:
+    def refresh_table_object(self, tbl_obj, session: Session = None):
+        """Refreshes a table object by adding it to the session, committing and refreshing it before
+        removing it from the session"""
+
+        def _refresh(sess: Session, tbl) -> Any:
             # Bind to session
-            session.add(tbl_obj)
+            sess.add(tbl)
             # Prime, pull down changes
-            session.commit()
+            sess.commit()
             # Populate changes to obj
-            session.refresh()
+            sess.refresh(tbl)
             # Remove obj from session
-            session.expunge(tbl_obj)
+            sess.expunge(tbl)
+
+        self.log.debug('Received request to refresh object...')
+        if session is None:
+            with self.session_mgr() as session:
+                tbl_obj = _refresh(sess=session, tbl=tbl_obj)
+        else:
+            # Working in an existing session
+            tbl_obj = _refresh(sess=session, tbl=tbl_obj)
         return tbl_obj
 
     def log_error_to_db(self, e: Exception, error_type: CahErrorType, player_key: int = None,
