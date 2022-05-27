931a976cefb4a9b09103f841f319a76c017213e2
barret <barret@tarkmail.com>
2022-05-27 10:35:22 -0500

    Add rips to table

---
+++ cah/model/rips.py
@@ -0,0 +1,31 @@
+import enum
+from sqlalchemy import (
+    Column,
+    Enum,
+    Integer,
+    VARCHAR
+)
+# local imports
+from cah.model.base import Base
+
+
+class RipType(enum.Enum):
+    DECKNUKE = enum.auto()
+
+
+class TableRip(Base):
+    """rip table - for storing rips the bot might make at others
+
+    Attributes:
+    """
+
+    rip_id = Column(Integer, primary_key=True, autoincrement=True)
+    rip_type = Column(Enum(RipType), nullable=False)
+    text = Column(VARCHAR(300), nullable=True)
+
+    def __init__(self, rip_type: RipType, text: str):
+        self.rip_type = rip_type
+        self.text = text
+
+    def __repr__(self) -> str:
+        return f'<TableRip(type={self.rip_type.name}, text={self.text})>'
