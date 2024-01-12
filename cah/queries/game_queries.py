298b8770a0d6dc764fa073704d5b4d5e5d9ba5be
Barret Obrock <barret@tarkmail.com>
2024-01-12 16:58:13 -0600

    Add initial game stats queries

--- cah/queries/game_queries.py
+++ cah/queries/game_queries.py
@@ -6,13 +6,18 @@ from typing import (
 )
 
 from loguru import logger
-from sqlalchemy.sql import func
+import pandas as pd
+from sqlalchemy.sql import (
+    and_,
+    func,
+)
 
 from cah.db_eng import WizzyPSQLClient
 from cah.model import (
     RipType,
     TableAnswerCard,
     TableGameRound,
+    TablePlayer,
     TablePlayerPick,
     TableQuestionCard,
     TableRip,
@@ -98,3 +103,57 @@ class GameQueries:
             if rip is None:
                 return 'I....got nothing. Consider yourself spared from rippin this time.'
             return rip.text
+
+    def get_game_stats(self) -> Dict:
+        with self.eng.session_mgr() as session:
+            # round duration
+            round_stats = session.query(
+                func.avg(TableGameRound.end_time - TableGameRound.start_time).label('avg_round'),
+                func.min(TableGameRound.end_time - TableGameRound.start_time).label('min_round'),
+                func.max(TableGameRound.end_time - TableGameRound.start_time).label('max_round')
+            ).filter(and_(
+                TableGameRound.end_time.isnot(None)
+            )).all()
+            avg_round, min_round, max_round = round_stats[0]
+
+            # pick/choose stats
+            pick_stats_q = session.query(
+                TableGameRound.game_round_id,
+                TableGameRound.start_time.label('round_start'),
+                TablePlayer.display_name,
+                TablePlayerPick.created_date.label('pick_timestamp'),
+                (TablePlayerPick.created_date - TableGameRound.start_time).label('duration_before_pick'),
+                TableGameRound.end_time.label('round_end')
+            ).join(TablePlayerPick, TablePlayerPick.game_round_key == TableGameRound.game_round_id).\
+                join(TablePlayer, TablePlayerPick.slack_user_hash == TablePlayer.slack_user_hash).\
+                filter(and_(
+                    TableGameRound.end_time.isnot(None)
+                ))
+
+            pick_stats_df = pd.read_sql(pick_stats_q.statement, session.bind)
+            slowest_pick_idx = pick_stats_df['duration_before_pick'].idxmax()
+            slowest_pick = pick_stats_df.loc[slowest_pick_idx, 'duration_before_pick']
+            slowest_pick_player = pick_stats_df.loc[slowest_pick_idx, 'display_name']
+
+            fastest_pick_idx = pick_stats_df['duration_before_pick'].idxmin()
+            fastest_pick = pick_stats_df.loc[fastest_pick_idx, 'duration_before_pick']
+            fastest_pick_player = pick_stats_df.loc[fastest_pick_idx, 'display_name']
+
+            avg_pick_time = pick_stats_df['duration_before_pick'].mean()
+
+            # TODO: More stats
+            #   % of time that a winner is the judge next
+
+            return {
+                'Avg Round': avg_round,
+                'Min Round': min_round,
+                'Max Round': max_round,
+                'Slowest Pick': slowest_pick,
+                'Slowest Pickler': slowest_pick_player,
+                'Fastest Pick': fastest_pick,
+                'fastest pickler': fastest_pick_player,
+                'avg pick time': avg_pick_time,
+                'mostest averagest pickler': '????',
+                'weirdest pickler': 'whichever name is barry\'s',
+                '% winner next round judge': 'TBD'
+            }
